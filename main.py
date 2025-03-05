a6e535d9a308825675dee09215fbf2b5d0c29398
WKroese <w_kroese@hotmail.com>
2025-03-05 15:56:21 +0100

    formatting of main

--- main.py
+++ main.py
@@ -12,13 +12,13 @@ import netCDF4 as nc
 import requests
 from datetime import datetime
 from datetime import timedelta
-import matplotlib
 import matplotlib.dates as mdates
-import windrose
 from windrose import WindroseAxes
-import config
+from dateutil import tz
 
-# plt.style.use('my_style')
+import config #contains api key
+
+plt.style.use('my_style') #custom style
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -59,7 +59,7 @@ def download_recent(dataset_name, dataset_version,data_dir):
     api_key = config.key 
     logger.info(f"Fetching files of {dataset_name} version {dataset_version}")
     api = OpenDataAPI(api_token=api_key)
-    
+
     begin = (datetime.today()-timedelta(hours=48)).strftime("%Y-%m-%dT%H:%M:%S+00:00")
     end = datetime.today().strftime("%Y-%m-%dT%H:%M:%S+00:00")
 
@@ -90,8 +90,7 @@ def make_plots(data_dir,fig_dir):
     date = ds['date'][:]
     t_hour = ds['time'][:]
     t = pd.to_datetime(date,format='%Y%m%d')+pd.to_timedelta(t_hour,'h')
-
-    now = datetime.now()
+    now = pd.Timestamp.now()
     mask = (t<now) & (t>now-timedelta(hours=36))
 
     T = ds['TA'][mask,:]-273.15
@@ -99,7 +98,6 @@ def make_plots(data_dir,fig_dir):
     F = ds['F'][mask,:]
     vis = ds['ZMA'][mask,:]
     D = ds['D'][mask,:]
-
     RH = 100*(np.exp((17.625*TD)/(243.04+TD))/np.exp((17.625*T)/(243.04+T))) 
 
     t = t[mask]
@@ -107,31 +105,28 @@ def make_plots(data_dir,fig_dir):
     plt.figure(figsize=(7,4))
     plt.plot(t,RH[:,-1])
     plt.title('relative humidity at 2 m \n'+'$RH_{min}$'+'={:.1f}, '.format(np.min(RH))+'$RH_{max}$'+'={:.1f}'.format(np.max(RH)))
-    plt.xlabel('time [hours]')
+    plt.xlabel('time UTC [hours]')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H"))
     plt.ylabel('RH [%]')
     plt.ylim(25,105)
-
     plt.savefig(fig_dir+'rh.png')
 
     plt.figure(figsize=(7,4))
     plt.plot(t,T[:,-1],label='T')
     plt.plot(t,TD[:,-1],label='dewpoint')
     plt.title('temperature and dewpoint at 2m \n'+'$T_{min}$'+'={:.1f}, '.format(np.min(T))+'$T_{max}$'+'={:.1f}'.format(np.max(T)))
-    plt.xlabel('time [hours]')
+    plt.xlabel('time UTC [hours]')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H"))
     plt.ylabel('T [$^\circ$C]')
-
     plt.legend()
     plt.savefig(fig_dir+'temp.png')
-
 
     plt.figure(figsize=(7,4))
     plt.plot(t,F[:,-2],label='10m')
     plt.plot(t,F[:,2],label='80m')
-    plt.plot(t,F[:,0],label='100m')
+    plt.plot(t,F[:,0],label='200m')
     plt.title('wind speed')
-    plt.xlabel('time [hours]')
+    plt.xlabel('time UTC [hours]')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H"))
     plt.ylabel('uv [m/s]')
     plt.legend()
@@ -142,7 +137,7 @@ def make_plots(data_dir,fig_dir):
     plt.scatter(t,D[:,2],label='80m',s=7)
     plt.scatter(t,D[:,0],label='200m',s=7)
     plt.title('wind direction')
-    plt.xlabel('time [hours]')
+    plt.xlabel('time UTC [hours]')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H"))
     plt.ylabel('degrees')
     plt.ylim(-5,365)
@@ -162,8 +157,9 @@ def make_plots(data_dir,fig_dir):
     plt.plot(t,vis[:,-1]*1e-3,label='2m')
     plt.plot(t,vis[:,2]*1e-3,label='80m')
     plt.plot(t,vis[:,0]*1e-3,label='200m')
+    
     plt.title('visibility')
-    plt.xlabel('time [hours]')
+    plt.xlabel('time UTC [hours]')
     plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%H"))
     plt.ylabel('vis [km]')
     plt.legend()
